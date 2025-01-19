760494fb92fb9f1cfd7c7a75a374a4954d180ee3
Toshiki-Nagahama <toshiki.nagahama@tanita.co.jp>
2025-01-19 21:19:41 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { useState, useEffect, useRef, BaseSyntheticEvent } from "react";
-import { invoke } from "@tauri-apps/api/core";
-import { listen } from "@tauri-apps/api/event";
+import { useState, useEffect, useRef } from "react";
 import "./App.css";
+import { createInitFiles, readMemo, saveMemo } from "./func.tsx";
 import { getCurrentWindow } from "@tauri-apps/api/window";
-import { exists, mkdir, create, BaseDirectory } from "@tauri-apps/plugin-fs";
 import { register } from "@tauri-apps/plugin-global-shortcut";
-import {
-  writeText,
-  readText,
-  readImage,
-} from "@tauri-apps/plugin-clipboard-manager";
+import clipboard from "tauri-plugin-clipboard-api";
 
 function App() {
   const appWindow = getCurrentWindow();
@@ -17,35 +11,22 @@ function App() {
   const [saveTimeStr, setSaveTimeStr] = useState<string>("");
   const [textAreaContent, setTextAreaContent] = useState<string>("");
   const [selectedTabNo, setSelectedTabNo] = useState<number>(0);
-  const refPrevClipboard = useRef<String>("");
-  const refClipboards = useRef([]);
-  const [clipboards, setClipboards] = useState([]);
-  const refPrevClipboardImage = useRef<Image>(null);
-  const refClipboardsImage = useRef([]);
-  const [clipboardsImage, setClipboardsImage] = useState([]);
+  const refPrevClipboard = useRef<string>("");
+  const tmpList1: string[] = [];
+  const tmpList2: string[] = [];
+  const tmpList3: string[] = [];
+  const tmpList4: string[] = [];
+  const refClipboards = useRef<string[]>(tmpList1);
+  const [clipboards, setClipboards] = useState<string[]>(tmpList2);
+  const refPrevClipboardImageStr = useRef<string>("");
+  const refClipboardsImageStr = useRef<string[]>(tmpList3);
+  const [clipboardsImageStr, setClipboardsImageStr] =
+    useState<string[]>(tmpList4);
   const refIsTop = useRef(false);
   refIsTop.current = isTop;
   const refTextArea = useRef<HTMLTextAreaElement>(null!);
   const TAB_MEMO = 0;
   const TAB_CLIPBOARD = 1;
-
-  function base64encode(data: Uint8Array) {
-    return btoa([...data].map((n) => String.fromCharCode(n)).join(""));
-  }
-  function arrayBufferToBase64(buffer, callback) {
-    console.log(buffer);
-    const blob = new Blob([buffer], {
-      type: "application/octet-binary",
-    });
-    const reader = new FileReader();
-    reader.onload = function (evt) {
-      const dataurl = evt.target.result;
-      console.log(dataurl);
-      console.log(dataurl.substr(dataurl.indexOf(",") + 1));
-      callback(dataurl.substr(dataurl.indexOf(",") + 1));
-    };
-    reader.readAsDataURL(blob);
-  }
 
   const keyEventListener = (e: KeyboardEvent) => {
     if (e.ctrlKey) {
@@ -55,67 +36,21 @@ function App() {
         setIsTop(!refIsTop.current);
       } else if (e.keyCode === 83) {
         //S
-        saveMemo();
+        saveMemo(refTextArea.current.value);
+        const d = new Date();
+        setSaveTimeStr(
+          `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} saved`,
+        );
       }
-    } else if (e.key === "Escape") {
-      refTextArea.current.blur();
     }
-
     if (e.metaKey) {
       if (e.keyCode === 83) {
         //S
-        saveMemo();
-      }
-    }
-  };
-
-  const saveMemo = async () => {
-    {
-      const file = await create("hama_sticky_notes/memos/tmp.txt", {
-        baseDir: BaseDirectory.Home,
-      });
-      file.write(new TextEncoder().encode(refTextArea.current.value));
-      file.close();
-    }
-    const d = new Date();
-    setSaveTimeStr(`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} saved`);
-  };
-
-  const onMouseDownTextArea = (e: BaseSyntheticEvent) => {
-    refTextArea.current.focus();
-  };
-
-  const createInitFiles = async () => {
-    {
-      //ベースディレクトリ
-      const dirExists = await exists("hama_sticky_notes", {
-        baseDir: BaseDirectory.Home,
-      });
-      if (!dirExists)
-        await mkdir("hama_sticky_notes", {
-          baseDir: BaseDirectory.Home,
-        });
-    }
-    {
-      //ファイルほぞｎディレクトリ
-      const dirExists = await exists("hama_sticky_notes/memos", {
-        baseDir: BaseDirectory.Home,
-      });
-      if (!dirExists)
-        await mkdir("hama_sticky_notes/memos", {
-          baseDir: BaseDirectory.Home,
-        });
-    }
-    {
-      const fileExists = await exists("hama_sticky_notes/config.json", {
-        baseDir: BaseDirectory.Home,
-      });
-      if (!fileExists) {
-        const file = await create("hama_sticky_notes/config.json", {
-          baseDir: BaseDirectory.Home,
-        });
-        file.write(new TextEncoder().encode("Hello world"));
-        file.close();
+        saveMemo(refTextArea.current.value);
+        const d = new Date();
+        setSaveTimeStr(
+          `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()} saved`,
+        );
       }
     }
   };
@@ -133,45 +68,54 @@ function App() {
 
   useEffect(() => {
     document.addEventListener("keydown", keyEventListener);
+    clipboard.startMonitor();
 
+    clipboard.onClipboardUpdate(async () => {
+      if (await clipboard.hasText()) {
+        const text = await clipboard.readText();
+        if (!refClipboards.current.includes(text)) {
+          //base64のimage/pngは飛ばす
+          if (!text.includes("data:image/png;base64,")) {
+            refPrevClipboard.current = text;
+            setClipboards([...refClipboards.current, text]);
+            refClipboards.current.push(text);
+          }
+        }
+      } else if (await clipboard.hasImage()) {
+        const base64img = await clipboard.readImageBase64();
+        const src = "data:image/png;base64," + base64img;
+        if (!refClipboardsImageStr.current.includes(src)) {
+          refPrevClipboardImageStr.current = src;
+          setClipboardsImageStr([...refClipboardsImageStr.current, src]);
+          refClipboardsImageStr.current.push(src);
+        }
+      }
+    });
     (async () => {
       createInitFiles();
       registerShortCuts();
-      setInterval(() => {
-        const polling = async () => {
-          try {
-            const clipboard = await readText();
-            if (refPrevClipboard.current != clipboard) {
-              console.log(clipboard);
-              refPrevClipboard.current = clipboard;
-              setClipboards([...refClipboards.current, clipboard]);
-              refClipboards.current.push(clipboard);
-            }
-          } catch {
-            //const clipboard = await readImage();
-            //console.log(clipboard);
-            //const src =
-            //  "data:image/png;charset=utf-8;base64,   " +
-            //  base64encode(await clipboard.rgba());
-            //if (refPrevClipboardImage.current != src) {
-            //  console.log(src);
-            //  refPrevClipboardImage.current = src;
-            //  setClipboardsImage([...refClipboardsImage.current, src]);
-            //  refClipboardsImage.current.push(src);
-            //}
-          }
-        };
-        polling();
-      }, 1000);
+      //メモファイルの内容読込
+      const content = await readMemo();
+      setTextAreaContent(content);
     })();
   }, []);
 
+  const onClickClipboard = (index: number) => {
+    clipboard.writeText(clipboards[index]);
+    setSaveTimeStr(`copied`);
+  };
+
+  const onClickClipboardImage = (index: number) => {
+    clipboard.writeText(clipboardsImageStr[index]);
+    setSaveTimeStr(`copied`);
+  };
+
   return (
-    <main className="w-full h-[calc(100vh-20px)] flex flex-col justify-start text-left overflow-scroll">
+    <main className="w-full h-[calc(100vh-20px)] flex flex-col justify-start text-left overflow-scroll hidden-scrollbar">
       <ul className="flex flex-wrap text-sm font-medium text-center border-b border-gray-700 text-gray-400">
         <li className="me-2">
           <a
-            onClick={(e) => {
+            onClick={() => {
               setSelectedTabNo(TAB_MEMO);
             }}
             className={`inline-block py-2 px-8 rounded-t-lg hover:bg-gray-800 hover:text-gray-300 
@@ -184,7 +128,7 @@ function App() {
         </li>
         <li className="me-2">
           <a
-            onClick={(e) => {
+            onClick={() => {
               setSelectedTabNo(TAB_CLIPBOARD);
             }}
             className={`inline-block py-2 px-4 rounded-t-lg hover:bg-gray-800 hover:text-gray-300 
@@ -209,21 +153,42 @@ function App() {
         ></textarea>
       )}
       {selectedTabNo === TAB_CLIPBOARD && (
-        <div>
+        <div className="">
           <button
-            className="px-4 py-2 bg-gray-800 mb-8"
+            className="px-4 py-2 bg-white text-black mt-2 ml-2 mb-4 rounded-sm"
             onClick={() => {
               setClipboards([]);
               refPrevClipboard.current = "";
               refClipboards.current = [];
+              setClipboardsImageStr([]);
+              refPrevClipboardImageStr.current = "";
+              refClipboardsImageStr.current = [];
+              clipboard.clear();
             }}
           >
-            clean
+            clear
           </button>
           {clipboards.map((s, index) => (
-            <div>
+            <div
+              key={index}
+              className="p-2 m-2 bg-[#ffffffb0] text-black rounded-md"
+              onClick={() => {
+                onClickClipboard(index);
+              }}
+            >
               <p key={index}>{s}</p>
               <br></br>
+            </div>
+          ))}
+          {clipboardsImageStr.map((s, index) => (
+            <div
+              key={index}
+              className="p-2 m-2 bg-[#ffffffb0] flex flex-row justify-center rounded-md"
+              onClick={() => {
+                onClickClipboardImage(index);
+              }}
+            >
+              <img src={s} key={index} />
             </div>
           ))}
         </div>
